e66b6add5436cb0e7c2fc1a195c53edbf6c6cb46
Abdallah <abdo.ashraf732111@gmail.com>
2021-12-16 15:39:19 +0200

    Meal Suggestions page First Articles page Meal description page Articles description page

---
+++ app/src/main/java/com/example/dms/Meal_pg.java
@@ -0,0 +1,15 @@
+package com.example.dms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Meal_pg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_pg);
+        getSupportActionBar().hide();
+    }
+}
