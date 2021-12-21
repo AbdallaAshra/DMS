3c94ea8a79a1390ef87a3db7db028bfd4d3b4c61
abdo faheem <55807719+AMF10@users.noreply.github.com>
2021-12-21 17:49:58 +0200

    Login Activity

--- app/src/main/java/com/example/dms/Article/Article_pg.java
+++ app/src/main/java/com/example/dms/Article/Article_pg.java
@@ -1,15 +1,17 @@
-package com.example.dms;
+package com.example.dms.Article;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Meal_pg extends AppCompatActivity {
+import com.example.dms.R;
+
+public class Article_pg extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meal_pg);
+        setContentView(R.layout.activity_article_pg);
         getSupportActionBar().hide();
     }
 }
